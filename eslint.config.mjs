187cfcd9a6f98ff2640fcf720ecdb8d73cc2347f
Ashukr321 <coderashukr321@gmail.com>
2026-08-29 23:54:46 +0530

    chore(package): upgrade package version and fix warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
   ...nextVitals,
   ...nextTs,
   prettier,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+];
 
 export default eslintConfig;
